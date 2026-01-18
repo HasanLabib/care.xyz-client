0185e29da786c7c8695899df338afc6c3cc5575e
HasanLabib <hasanlabib3@gmail.com>
2026-01-18 21:32:04 +0600

    vercel error handle"

--- src/app/library/api.js
+++ src/app/library/api.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: "https://server-psi-lake-59.vercel.app",
+  withCredentials: true,
 });
 
 api.interceptors.request.use((config) => {
